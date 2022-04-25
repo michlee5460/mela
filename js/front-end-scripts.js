7aa1436a00c4afe0c9ac5a69f1c545c96369a3b7
michlee5460 <michlee5460@gmail.com>
2022-04-25 10:15:17 -0400

    starting to integrate tumor board page

---
+++ js/front-end-scripts.js
@@ -0,0 +1,38 @@
+// drag and drop table rows
+// reference: https://bootsnipp.com/snippets/P2pn5
+var fixHelperModified = function(e, tr) {
+    var $originals = tr.children();
+    var $helper = tr.clone();
+    $helper.children().each(function(index) {
+        $(this).width($originals.eq(index).width())
+    });
+    return $helper;
+},
+    updateIndex = function(e, ui) {
+        $('td.index', ui.item.parent()).each(function (i) {
+            $(this).html(i+1);
+        });
+        $('input[type=text]', ui.item.parent()).each(function (i) {
+            $(this).val(i + 1);
+        });
+    };
+
+$("#april-4th-table tbody").sortable({
+    helper: fixHelperModified,
+    stop: updateIndex
+}).disableSelection();
+
+    $("tbody").sortable({
+    distance: 5,
+    delay: 100,
+    opacity: 0.6,
+    cursor: 'move',
+    update: function() {}
+        });
+
+
+
+// Data Picker Initialization
+$('.datepicker').datepicker({
+    inline: true
+  });
